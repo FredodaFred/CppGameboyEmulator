e1c81720c9f941de8cb2e7b2435946ca956d9ae4
Frederick Sion <62347411+FredodaFred@users.noreply.github.com>
2026-03-11 20:38:12 -0400

    minor screen sizing adjustment

--- emu_core/src/graphics/screen.hpp
+++ emu_core/src/graphics/screen.hpp
@@ -7,6 +7,9 @@
 #include <format>
 #include "../log/logger.hpp"
 
+#define DMG_WIDTH 160
+#define DMG_HEIGHT 144
+
 class Screen {
     public:
         Screen() = default;
@@ -22,8 +25,8 @@ class Screen {
         GLuint shader_program;  // shader for openGL (we don't actually use this, the library forces us to make it)
 
         static constexpr uint8_t rgb_palette[4][3] = {{255, 255, 255}, {211, 211, 211}, {169, 169, 169}, {0, 0, 0}};
-        static constexpr int windowWidth = 160;
-        static constexpr int windowHeight = 144;
+        static constexpr int windowWidth = DMG_WIDTH * 3;
+        static constexpr int windowHeight = DMG_HEIGHT * 3;
         void draw_pixel_to_buffer(uint8_t *curr_pixels, int idx, uint8_t pixel);
         void compile_shaders();
 };
